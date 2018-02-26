5cb9adf50e9a4f06d7c11a0e81efff3720ffbb9d
Yang Liu <lykavin@hotmail.com>
2018-02-26 15:54:23 +0800

    finished player movement & simple tilemap

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(rb.velocity.y < 0)
+        {
+            rb.velocity += Physics2D.gravity * (fallMultiplier - 1f) * Time.deltaTime;
+        } else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+	}
+}
